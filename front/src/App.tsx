8da964bdb69f6a0187dd7353c1b519f99c7a9063
falabrettir <falabretti@alunos.utfpr.edu.br>
2025-10-06 21:57:06 -0300

    a

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { Button } from './components/ui/button'
 import './App.css'
 
 function App() {
   return (
-    <button>
+    <Button>
       Click me!
-    </button>
+    </Button>
   )
 }
 
